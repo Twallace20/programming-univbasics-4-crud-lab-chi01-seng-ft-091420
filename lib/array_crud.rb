4b0834b6e31ce522bf0b2bd8dbcc56f54645410a
Tyler Wallace <tywallace2017@gmail.com>
2020-08-26 18:52:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,34 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Hi", "I", "Am", "Tyler"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = ["Hooray"]
+add_element_to_end_of_array.push ("arrays!")
+p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array = ["ItsTwoChains"]
+  add_element_to_start_of_array.unshift ("wow")
+  p add_element_to_start_of_array
 end
 
 def remove_element_from_end_of_array(array)
-  
+ remove_element_from_end_of_array =["nice","arrays!"]
+ "arrays!" = remove_element_from_end_of_array.shift 
+ p remove_element_from_end_of_array
+ 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift = "array"
+  p remove_element_from_start_of_array
+  p array 
 end
 
 def retrieve_element_from_index(array, index_number)
