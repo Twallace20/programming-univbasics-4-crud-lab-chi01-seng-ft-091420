7a7f88fe60887f4697ac00b6f6a20149c469d3ce
Tyler Wallace <tywallace2017@gmail.com>
2020-08-26 18:58:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,9 +20,9 @@ end
 
 def remove_element_from_end_of_array(array)
  remove_element_from_end_of_array =["nice","arrays!"]
- arrays!_array  = remove_element_from_end_of_array.shift 
+ arrays_array  = remove_element_from_end_of_array.shift 
  p remove_element_from_end_of_array
- 
+ p arrays_array
 end
 
 def remove_element_from_start_of_array(array)
